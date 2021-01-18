5b921be43c95c1668854a80c423a7de0f8083b41
izzyspencer98 <56084472+izzyspencer98@users.noreply.github.com>
2021-01-18 14:13:21 +0000

    sayhello component - using props

--- ReviewProject/App.js
+++ ReviewProject/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+class SayHello extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello {this.props.name}</Text>
+      </View>
+    )
+  }
+}
+
+
 class HelloWorldApp extends Component {
   render() {
-    return(
+    return (
       <View>
-        <Text>Hello World</Text>
+        <SayHello name="Izzy" />
       </View>
     );
   }
 }
 
- export default HelloWorldApp;
+export default HelloWorldApp;
