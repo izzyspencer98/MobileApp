1baa6448d9355241db792a8b0330c3cc48b08910
izzyspencer98 <56084472+izzyspencer98@users.noreply.github.com>
2021-01-25 12:54:50 +0000

    installed eslint

--- ReviewProject/App.js
+++ ReviewProject/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+// import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-class SayHello extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Hello {this.props.name}</Text>
-      </View>
-    )
-  }
+const Stack = createStackNavigator()
+
+const App = () => {
+  return (
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  )
 }
-
-
-class HelloWorldApp extends Component {
-  render() {
-    return (
-      <View>
-        <SayHello name="Izzy" />
-      </View>
-    );
-  }
-}
-
-export default HelloWorldApp;
+export default App
